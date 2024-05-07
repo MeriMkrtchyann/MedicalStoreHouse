73fcffb172e14d0a1743a8f958e0616173488fa1
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2024-05-07 17:06:47 +0400

    add paymoent form

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
           <Route path="/aboutProduct" element={product ? <Product product={product} activeUser={activeUser} setActiveUser={setActiveUser} setProduct={setProduct} setActiveCategory={setActiveCategory} basket={basket} setBasket={setBasket} sum={sum} setSum={setSum}/> : <NotFoundPage/>}/>
           <Route path="/forgetPassword" element={<PassResetPage/>}/>
           <Route path="/admin" element={admin ? <AdminPage admin={admin} setAdmin={setAdmin}/> : <NotFoundPage/>}/>
-          <Route path="/payment" element={<PaymentPage/>}/>
+          <Route path="/payment" element={<PaymentPage sum={sum}/>}/>
       </Routes>
     </>
   );
--- src/pages/paymentPage/PaymentPage.jsx
+++ src/pages/paymentPage/PaymentPage.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './PaymentPage.css'; 
 
-const PaymentForm = () => {
+const PaymentForm = ({sum}) => {
+
+  const years = [];
+  const manths = []
+
+  for (let year = 2024; year <= 2034; year++) {
+    years.push(year);
+  }
+
+  for (let manth = 0; manth <= 12; manth++) {
+    manths.push(manth);
+  }
+
   return(
     <div className="paymentPage">
       <div className="paymentPageConteyner">
@@ -10,7 +22,7 @@ const PaymentForm = () => {
             <img src ={process.env.PUBLIC_URL + "img/ameriabank.svg"} alt="logo"/> 
           </div>
           <div className="timeEnded">
-            <p>Աշխատանքի վարտին մնացել է </p>
+            <p>Աշխատանքի ավրտին մնացել է </p>
           </div>
         </div>
         <div className="paymentConteyner">
@@ -18,13 +30,49 @@ const PaymentForm = () => {
             <p>Խնդրում եմ լրացել Ձեր վճարային քարտի տվյալները</p>
           </div>
           <div className="paymentForm">
-            <h3>Հաշվի նկարագրություն</h3>
-            <h5>Medical Store House</h5>
-            <h3>Վճարի մեծություն</h3>
-            <p>100.00 AMD</p>
-            <h3>Ձեր քարտի համարը</h3>
-            <p>16 նիշ առանց բացատների</p>
-            
+            <div>
+              <h3>Հաշվի նկարագրություն</h3>
+              <p>Medical Store House</p>
+            </div>
+            <div>
+              <h3>Վճարի մեծություն</h3>
+              <div className="sum">
+                <span>{sum}</span>
+              </div>
+            </div>
+            <div>
+              <h3>Ձեր քարտի համարը</h3>
+              <p>16 նիշ առանց բացատների</p>
+              <input type="text" name="cardNumber"></input>
+            </div>
+            <div>
+              <h3>Ձեր անունը</h3>
+              <p>Ինչպես գրված է քարտի վրա (օր MR NAME SURNAME)</p>
+              <input type="text" name="cardNumber"></input>
+            </div>
+            <div>
+              <h3>Քարտի գործողության վերջնաժամկետը</h3>
+              <div className="manthAndYear">
+                <select id="manth">
+                  {manths.map((manth) => (
+                    <option key={manth} value={manth}>{manth}</option>
+                  ))}
+                </select>
+                <select id="year">
+                  {years.map((year) => (
+                    <option key={year} value={year}>{year}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div>
+              <h3>CVC2/ CVV2 կոդ</h3> 
+              <p>Եռանիշ թիվ, որը գտնվում է քարտի հակառակ կողմում</p>
+              <input type="text" name="cvcNumber"></input>
+            </div>
+            <div class="paymentButton">
+              <button>ՎՃԱՐԵԼ</button>
+            </div>
           </div>
         </div>
       </div>
